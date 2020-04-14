acc8bec0f1a0ebdb7b7151a02b8240ee3ee7066a
kate_gaw <ekaterinagaw@gmail.com>
2020-04-14 13:41:59 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,34 @@
+const randomPointX = (arr) => arr[Math.floor(Math.random() * (arr.length))];
+
+const randomPointY = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
+
+const getPoint = () => {
+  const arr = [0, 2, 4, 6];
+  const i = randomPointX(arr);
+  const j = randomPointY(0, 3);
+
+  return (document.getElementById('table').childNodes[1].childNodes[i].cells[j]);
+};
+
+const setPicture = () => {
+  const image = document.createElement('img');
+  image.src = '/src/img/goblin.png';
+  image.style.width = '50%';
+  image.style.height = 'auto';
+
+  let end;
+
+  const timerId = setInterval(() => {
+    const point = getPoint();
+    point.appendChild(image);
+    end = point;
+  }, 1000);
+
+  setTimeout(() => {
+    clearInterval(timerId);
+    end.innerHTML = '';
+  }, 1000000);
+};
+
+getPoint();
+setPicture();
