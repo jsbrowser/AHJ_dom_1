afcf9395e6a664f2cf5f7b8e6a1ad344c5fda627
kate_gaw <ekaterinagaw@gmail.com>
2020-04-14 14:06:38 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,31 +3,31 @@ const randomPointX = (arr) => arr[Math.floor(Math.random() * (arr.length))];
 const randomPointY = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
 
 const getPoint = () => {
-    const arr = [0, 2, 4, 6];
-    const i = randomPointX(arr);
-    const j = randomPointY(0, 3);
+  const arr = [0, 2, 4, 6];
+  const i = randomPointX(arr);
+  const j = randomPointY(0, 3);
 
-    return (document.getElementById('table').childNodes[0].childNodes[i].cells[j]);
+  return (document.getElementById('table').childNodes[0].childNodes[i].cells[j]);
 };
 
 const setPicture = () => {
-    const image = document.createElement('img');
-    image.src = '/src/img/goblin.png';
-    image.style.width = '50%';
-    image.style.height = 'auto';
+  const image = document.createElement('img');
+  image.src = '/src/img/goblin.png';
+  image.style.width = '50%';
+  image.style.height = 'auto';
 
-    let end;
+  let end;
 
-    const timerId = setInterval(() => {
-        const point = getPoint();
-        point.appendChild(image);
-        end = point;
-    }, 1000);
+  const timerId = setInterval(() => {
+    const point = getPoint();
+    point.appendChild(image);
+    end = point;
+  }, 1000);
 
-    setTimeout(() => {
-        clearInterval(timerId);
-        end.innerHTML = '';
-    }, 1000000);
+  setTimeout(() => {
+    clearInterval(timerId);
+    end.innerHTML = '';
+  }, 1000000);
 };
 
 getPoint();
