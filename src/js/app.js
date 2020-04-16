f2233027b1b3dcf0d5f415d89236ab7c013d1159
kate_gaw <ekaterinagaw@gmail.com>
2020-04-16 12:45:54 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,36 @@
 /* eslint-disable no-unused-vars */
 function randomPointX(arr) {
-    return arr[Math.floor(Math.random() * (arr.length))];
+  return arr[Math.floor(Math.random() * (arr.length))];
 }
 
 function randomPointY(min, max) {
-    return Math.floor(min + Math.random() * (max + 1 - min));
+  return Math.floor(min + Math.random() * (max + 1 - min));
 }
 
 function getPoint() {
-    const arr = [0, 1, 2, 3];
-    const i = randomPointX(arr);
-    const j = randomPointY(0, 3);
-    return (document.getElementById('table').rows[i].cells[j]);
+  const arr = [0, 1, 2, 3];
+  const i = randomPointX(arr);
+  const j = randomPointY(0, 3);
+  return (document.getElementById('table').rows[i].cells[j]);
 }
 
 
 function setPicture() {
-    const image = document.createElement('img');
-    image.src = './goblin.png';
-    image.style.width = '50%';
-    image.style.height = 'auto';
+  const image = document.createElement('img');
+  image.src = './goblin.png';
+  image.style.width = '50%';
+  image.style.height = 'auto';
 
-    let point = getPoint();
-    let end;
+  let point = getPoint();
+  let end;
 
-    setInterval(() => {
-        if (point === end) {
-            do {
-                point = getPoint();
-            } while (point === end);
-        }
-        point.appendChild(image);
-        end = point;
-    }, 1000);
+  setInterval(() => {
+    if (point === end) {
+      do {
+        point = getPoint();
+      } while (point === end);
+    }
+    point.appendChild(image);
+    end = point;
+  }, 1000);
 }
